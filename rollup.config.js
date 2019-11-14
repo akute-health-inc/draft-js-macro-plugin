ccf97bb1d2c6f8f1c8b209ec05bb0bed170b77b4
Sharud Agarwal <agarwal.sharud@gmail.com>
2019-11-13 18:26:37 -0600

    finally builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,40 @@
-import path from 'path';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import path from "path";
+import nodeResolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
 
-const input = './src/index.js';
-const external = id => !id.startsWith('.') && !path.isAbsolute(id);
+const input = "./src/index.js";
+const external = id => !id.startsWith(".") && !path.isAbsolute(id);
 
 export default [
   {
     input,
     output: {
-      format: 'cjs',
-      file: './lib/index.cjs.js',
-      exports: 'named',
+      format: "cjs",
+      file: "./lib/index.cjs.js",
+      exports: "named"
     },
     external,
-    plugins: [nodeResolve(), babel({ rootMode: 'upward' })],
+    plugins: [nodeResolve(), babel()]
   },
   {
     input,
     output: {
-      format: 'esm',
-      file: './lib/index.esm.js',
+      format: "esm",
+      file: "./lib/index.esm.js"
     },
     external,
     plugins: [
       nodeResolve(),
       babel({
-        rootMode: 'upward',
         plugins: [
           [
-            'babel-plugin-transform-rename-import',
+            "babel-plugin-transform-rename-import",
             {
-              replacements: [{ original: 'lodash', replacement: 'lodash-es' }],
-            },
-          ],
-        ],
-      }),
-    ],
-  },
+              replacements: [{ original: "lodash", replacement: "lodash-es" }]
+            }
+          ]
+        ]
+      })
+    ]
+  }
 ];
